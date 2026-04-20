67c8fc97498a5cd86f78d21db965d0e7e4c92df5
mmgonnar <gn.mariela@gmail.com>
2026-04-20 13:35:45 -0600

    feat: :sparkles: add lucide-react and next-themes dependencies; enhance page layout with new button components

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,13 @@
+import { Button } from '@/components/ui/button';
+
 export default function Home() {
   return (
     <main>
       <div>Hello world!</div>
+      <Button>Click me</Button>
+      <Button variant="outline">Click me</Button>
+      <Button variant="danger">Click me</Button>
+      <Button variant="ghost">Click me</Button>
     </main>
   );
 }
