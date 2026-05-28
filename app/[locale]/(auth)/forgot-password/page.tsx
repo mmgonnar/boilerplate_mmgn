5ff0ea3fbe139c78c8e19fcbd75c10c2ead2e108
mmgonnar <gn.mariela@gmail.com>
2026-05-28 17:18:04 -0600

    refactor: remove unused LoginForm import from ForgotPassword page; streamline code for better readability

--- app/[locale]/(auth)/forgot-password/page.tsx
+++ app/[locale]/(auth)/forgot-password/page.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next';
 
-import { LoginForm } from '@/features/auth';
 import { ForgotPasswordForm } from '@/features/auth/components/forgot-password-form';
 
 export const metadata: Metadata = {
